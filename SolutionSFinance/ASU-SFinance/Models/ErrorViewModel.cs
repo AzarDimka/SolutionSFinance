216dc12d64da7cee1222a9613cf7e9abb3b5c56f
AzarovDU <AzarovDU@bukep.ru>
2025-02-25 10:45:35 +0300

    сделал переезд на 8net. пересоздал все проекты (возможно это было излишне)

--- SolutionSFinance/ASU-SFinance/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASU_SFinance.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ SolutionSFinance/SodruzhestvoFinance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SodruzhestvoFinance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
