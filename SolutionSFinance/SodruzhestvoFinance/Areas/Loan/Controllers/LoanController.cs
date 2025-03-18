309d016fc1ed2dc5c3d167dcea5d26bb9bb89300
AzarovDU <AzarovDU@bukep.ru>
2025-03-18 20:10:18 +0300

    что-то правил

--- SolutionSFinance/SodruzhestvoFinance/Areas/Loan/Controllers/LoanController.cs
+++ SolutionSFinance/SodruzhestvoFinance/Areas/Loan/Controllers/LoanController.cs
@@ -4,6 +4,7 @@ using SodruzhestvoFinance.Data;
 using SodruzhestvoFinance.Areas.Loan.Models;
 using SodruzhestvoFinance.Areas.Employees.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using SodruzhestvoFinance.Areas.Loan.Enum;
 
 
 namespace SodruzhestvoFinance.Areas.Loan.Controllers
@@ -28,24 +29,6 @@ namespace SodruzhestvoFinance.Areas.Loan.Controllers
         [HttpGet]
         public IActionResult LoanCreationPage()
         {
-            //// 1. Получение списка сотрудников из базы данных
-            //var employees = _context.Employees.ToList();
-
-            //// 2. Создание списка SelectListItem для DropDownList
-            //var employeeList = employees.Select(e => new SelectListItem
-            //{
-            //    Value = e.EmployeeId.ToString(),
-            //    Text = e.LastName // Или e.FirstName + " " + e.LastName, в зависимости от того, как вы храните имя
-            //}).ToList();
-
-            //// 3. Создание ViewModel и передача списка сотрудников
-            //var viewModel = new LoanIssueViewModel
-            //{
-            //    Employees = employeeList,
-            //    IssueDate = DateTime.Today //Установим значение по умолчанию
-            //};
-
-            // 4. Передача ViewModel в представление
             return View("Create");
         }
 
@@ -64,7 +47,7 @@ namespace SodruzhestvoFinance.Areas.Loan.Controllers
                     LoanTerm = model.LoanTerm,
                     IssueDate = model.IssueDate,
                     CurrentBalance = model.LoanAmount, // Изначально остаток равен сумме займа
-                    Status = "Активный" // Или используйте enum: LoanStatus.Active.ToString()
+                    Status = LoanStatusType.newLoan.ToString() // Или используйте enum: LoanStatus.Active.ToString()
                 };
 
                 // 2. Добавление займа в базу данных
@@ -86,5 +69,89 @@ namespace SodruzhestvoFinance.Areas.Loan.Controllers
 
             return View(model);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var loan = await _context.Loans.FindAsync(id);
+            if (loan == null)
+            {
+                return NotFound();
+            }
+            return View(loan);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSave(int id, LoanIssueViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var loan = await _context.Loans.FindAsync(id);
+                    if (loan == null)
+                    {
+                        return NotFound();
+                    }
+
+                    loan.EmployeeId = viewModel.EmployeeId;
+                    loan.LoanAmount = viewModel.LoanAmount;
+                    loan.InterestRate = viewModel.InterestRate;
+                    loan.LoanTerm = viewModel.LoanTerm;
+                    loan.IssueDate = viewModel.IssueDate;
+
+                    _context.Update(loan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LoanExists(viewModel.EmployeeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            viewModel.Employees = _context.Employees.Select(e => new SelectListItem
+            {
+                Value = e.EmployeeId.ToString(),
+                Text = e.LastName
+            }).ToList();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LoanExists(int id)
+        {
+            return _context.Loans.Any(e => e.LoanId == id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var loans = await _context.Loans.FindAsync(id);
+
+            if (loans == null)
+            {
+                //return Json(new { success = false, message = "Сотрудник не найден" }); // Возвращаем JSON с ошибкой
+                return NotFound();
+            }
+
+            _context.Loans.Remove(loans);
+            await _context.SaveChangesAsync();
+
+            //return Json(new { success = true, message = "Сотрудник успешно удален" }); // Возвращаем JSON об успехе
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
---
+++ SolutionSFinance/SodruzhestvoFinance/Areas/Loan/Enum/LoanStatusType.cs
@@ -0,0 +1,23 @@
+﻿namespace SodruzhestvoFinance.Areas.Loan.Enum
+{
+    /// <summary>
+    /// Тип статуса займа
+    /// </summary>
+    public enum LoanStatusType
+    {
+        /// <summary>
+        /// Новый
+        /// </summary>
+        newLoan = 1,
+
+        /// <summary>
+        /// Активный
+        /// </summary>
+        active = 2,
+
+        /// <summary>
+        /// Погащен
+        /// </summary>
+        repaid = 3
+    }
+}
